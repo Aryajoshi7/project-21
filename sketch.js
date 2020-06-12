96ed371363418de9294080baaf9a2d60eec14d95
Aryajoshi7 <65438211+Aryajoshi7@users.noreply.github.com>
2020-06-12 17:34:20 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ speed = random(223, 321);
 weight = random(30, 52);
 thickness = random(22, 83);
 
+
+
 bullet = createSprite(50, 200, 25, 10);
 wall = createSprite(1200, 200, thickness, 400/2);
 wall.shapeColor = color(80,80,80);
@@ -17,21 +19,34 @@ function draw() {
   background(255,255,255);  
 bullet.velocityX = speed;
 
+ 
 
 
-if(bullet.collide(wall)){
-  bullet.velocityX = 0;
-damage = (0.5*weight*speed*speed)/(thickness*thickness*thickness);
+hasCollided();
 
-if( damage<= 10){
-  wall.shapeColor = color("green");
-}else{
-  wall.shapeColor = color("red");
-}
-
-
-}
 
 
   drawSprites();
 }
+
+
+
+function hasCollided(){
+  if(bullet.collide(wall)){
+    bullet.velocityX = 0;
+  damage = (0.5*weight*speed*speed)/(thickness*thickness*thickness);
+ }
+
+
+  if( damage<= 10){
+  wall.shapeColor = color("green");
+}else{
+  wall.shapeColor = color("red");
+
+}
+
+
+
+}
+
+
